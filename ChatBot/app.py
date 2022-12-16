fc338bf12a7f26e1b9c82594316090529415a2cd
odulaja philip <odulajaphilip@gmail.com>
2022-12-16 19:47:19 +0100

    created templates and static template

---
+++ ChatBot/app.py
@@ -0,0 +1,11 @@
+from flask import Flask , request , render_template
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainers, ChatterBotCorpusTrainer
+
+
+app = Flask()
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
