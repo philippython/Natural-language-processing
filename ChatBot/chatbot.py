63c38ae8b054fbbf14c38486dc47a9f785d1a6b9
odulaja philip <odulajaphilip@gmail.com>
2022-12-17 18:14:46 +0100

    updated chatterbot logic and storage apdater

--- ChatBot/chatbot.py
+++ ChatBot/chatbot.py
@@ -2,7 +2,18 @@ from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer, ChatterBotCorpusTrainer
 
 # creating chatbot instance
-Vennote_Bot = ChatBot("VennoteBot", )
+Vennote_Bot = ChatBot("VennoteBot",storage_adapter='chatterbot.storage.SQLStorageAdapter',
+                logic_adapters=[
+                    'chatterbot.logic.MathematicalEvaluation',
+                    'chatterbot.logic.TimeLogicAdapter',
+                    'chatterbot.logic.BestMatch',
+                    {
+                        'import_path': 'chatterbot.logic.BestMatch',
+                        'default_response': 'I am sorry, but I do not understand. I am still learning.',
+                        'maximum_similarity_threshold': 0.90
+                    }
+                ],
+                database_uri='sqlite:///database.sqlite3' )
 
 # Training with Personal Ques & Ans 
 conversation = [
